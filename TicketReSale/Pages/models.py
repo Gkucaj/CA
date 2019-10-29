8693b06df246718ae3c3d488eba5ecdcdac6ce89
Gracjan <x00155111@mytudublin.ie>
2019-10-29 16:10:56 +0000

    added admin, models working for events, you can add an event throught the admin page

--- TicketReSale/Pages/models.py
+++ TicketReSale/Pages/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class event(models.Model):
+    event_name = models.CharField(max_length=200)
+    event_date = models.DateTimeField("Event date:")
+    event_venue = models.CharField(max_length=200)
+    event_image = models.ImageField(upload_to='images/', blank = True)
+    
+    def __str__(self):
+        return self.event_name
+
+
