c5b166c1af8b41d521bf422251d1c99fb0ff3e36
Gracjan <x00155111@mytudublin.ie>
2019-10-29 16:57:38 +0000

    8000/events shows up a page for the events, page not finished yet, will do tomorrow : 30/10

--- TicketReSale/Pages/admin.py
+++ TicketReSale/Pages/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from  .models import event
 
+admin.site.site_header = "TeX"
+admin.site.site_title = "TeX admin area"
+admin.site.site_index_title = "TeX admin"
+
 admin.site.register(event)
--- TicketReSale/Pages/views.py
+++ TicketReSale/Pages/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import event
+
+def index(request):
+    return render(request, 'Pages/index.html')
